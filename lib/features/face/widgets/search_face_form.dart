ac7cec78330e795ea8386ad5b5b1e754e4e50d4d
ch1nru5t <chinrust@gmail.com>
2023-04-26 11:31:58 +0530

    fixes

--- lib/features/face/widgets/search_face_form.dart
+++ lib/features/face/widgets/search_face_form.dart
@@ -41,78 +41,83 @@ class _SearchFaceFormState extends State<SearchFaceForm> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        Expanded(
-          child: GestureDetector(
-            onTap: () async {
-              image = await imagePicker.pickImage(source: ImageSource.camera);
-              setState(() {});
-            },
-            child: image != null
-                ? Column(
-                    children: [
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.8,
-                        height: MediaQuery.of(context).size.height * 0.59,
-                        child: Image.file(
-                          fit: BoxFit.contain,
-                          File(image!.path),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+        child:
+            Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          Expanded(
+            flex: 8,
+            child: GestureDetector(
+              onTap: () async {
+                image = await imagePicker.pickImage(source: ImageSource.camera);
+                setState(() {});
+              },
+              child: image != null
+                  ? Column(
+                      children: [
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          height: MediaQuery.of(context).size.height * 0.59,
+                          child: Image.file(
+                            fit: BoxFit.contain,
+                            File(image!.path),
+                          ),
                         ),
+                        const SizedBox(height: 10),
+                        IconButton(
+                          onPressed: () {
+                            setState(
+                              () {
+                                image = null;
+                              },
+                            );
+                          },
+                          icon: const Icon(Icons.delete_outline),
+                        ),
+                      ],
+                    )
+                  : DottedBorder(
+                      borderType: BorderType.RRect,
+                      radius: const Radius.circular(10),
+                      strokeCap: StrokeCap.round,
+                      dashPattern: const [10, 4],
+                      child: Container(
+                        width: double.infinity,
+                        height: MediaQuery.of(context).size.height * 0.62,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Icon(Icons.camera_outlined),
+                              const SizedBox(height: 15),
+                              Text(
+                                'Tap here to Open Camera',
+                                style: TextStyle(
+                                    fontSize: 15, color: Colors.grey.shade400),
+                              ),
+                            ]),
                       ),
-                      const SizedBox(height: 10),
-                      IconButton(
-                        onPressed: () {
-                          setState(
-                            () {
-                              image = null;
-                            },
-                          );
-                        },
-                        icon: const Icon(Icons.delete_outline),
-                      ),
-                    ],
-                  )
-                : DottedBorder(
-                    borderPadding: const EdgeInsets.all(20),
-                    borderType: BorderType.RRect,
-                    radius: const Radius.circular(10),
-                    strokeCap: StrokeCap.round,
-                    dashPattern: const [10, 4],
-                    child: Container(
-                      width: double.infinity,
-                      height: MediaQuery.of(context).size.height * 0.62,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10)),
-                      child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Icon(Icons.camera_outlined),
-                            const SizedBox(height: 15),
-                            Text(
-                              'Tap here to Open Camera',
-                              style: TextStyle(
-                                  fontSize: 15, color: Colors.grey.shade400),
-                            ),
-                          ]),
                     ),
-                  ),
-          ),
-        ),
-        SizedBox(
-          width: double.infinity,
-          child: TextButton(
-            style: TextButton.styleFrom(
-              padding: const EdgeInsets.symmetric(vertical: 20),
-              backgroundColor: Colors.blue,
-            ),
-            onPressed: searchImage,
-            child: const Text(
-              'Search',
-              style: TextStyle(color: Colors.white, fontSize: 20),
             ),
           ),
-        ),
-      ]),
+          const SizedBox(height: 10),
+          Expanded(
+            flex: 1,
+            child: TextButton(
+              style: TextButton.styleFrom(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                backgroundColor: Colors.blue,
+              ),
+              onPressed: searchImage,
+              child: const Text(
+                'Search',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ),
+          ),
+        ]),
+      ),
     );
   }
 }
